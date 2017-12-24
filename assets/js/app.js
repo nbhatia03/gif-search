cfab9469b901fac9c9e077000f727f58770758e3
tesh-bhatia <nbhatia03@gmail.com>
2017-12-23 20:44:08 -0500

    Appends still gifs to DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,10 +20,25 @@ function buttonClick(){
 
     var searchTerm = $(this).attr('data-name');
     var limit = 5;
-    var url = "http://api.giphy.com/v1/gifs/search?q="+ searchTerm + "&api_key=SUKDMODfO0RfDFQmemfkqV6EeKBCtnjK&limit=" + limit
+    var url = "http://api.giphy.com/v1/gifs/search?q="+ searchTerm + "&api_key=SUKDMODfO0RfDFQmemfkqV6EeKBCtnjK&rating+R&limit=" + limit
     $.get(url).done(function(response){
         console.log(response);
+        updateDom(response);
     })
+}
+
+function updateDom(response){
+    $('#gifs').empty();
+    var objArray = response.data
+    objArray.forEach(function(gif){
+        var imgSrc = gif.images.fixed_height_still.url;
+        var newDiv = $('<div>')
+        newDiv.addClass('gif')
+        .append('<img src="' + imgSrc + '"/>') //change to fixed_height onClick
+        $('#gifs').append(newDiv)
+    }
+        
+    );
 }
 
 $('#buttons').on('click', '.btn', buttonClick);
